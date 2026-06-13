346ba9070f50a16aaf6ab5ce80c21939952b851b
Nikhil Sharma <me@nikhil.sh>
2026-06-13 15:01:16 +0800

    android: import collectAsState in AgentLoginSheet (compile fix)

--- apps/android/app/src/main/kotlin/sh/nikhil/conduit/ui/AgentLoginSheet.kt
+++ apps/android/app/src/main/kotlin/sh/nikhil/conduit/ui/AgentLoginSheet.kt
@@ -35,6 +35,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
